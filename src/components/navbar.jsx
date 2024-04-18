2a765a579b2ade8fc8b9b47586dfe35700254e3c
ajavieralejandro <amorosijavier@gmail.com>
2024-04-18 19:09:22 -0300

    Updating for deploy

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () =>{
         <div>
         <nav class="flex items-center justify-between flex-wrap  p-6">
   <div class="flex items-center flex-shrink-0 text-black mr-6">
-    <span class="font-semibold text-xl tracking-tight">Audiometrias Carnet Conducir y Audifonos</span>
+    <span class="font-semibold text-3xl tracking-tight">Audiometrias Carnet Conducir y Audifonos</span>
   </div>
 
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://ajavieralejandro.github.io/reactTailwindViteTemplate"
+  base : "https://audifonosyaudiometriascarnetconducir.com"
 })
